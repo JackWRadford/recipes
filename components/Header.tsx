ace31e6de204445d4f32cdeb926a6ea20f14f82c
JackR <jack.radford@live.co.uk>
2022-09-24 15:05:39 +0100

    Added Link on title in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,11 @@ const Header = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <div className={styles.logoContainer}>
-          <h1>🥘 Recipes</h1>
-        </div>
+        <Link href={"/"}>
+          <div className={styles.logoContainer}>
+            <h1>🥘 Recipes</h1>
+          </div>
+        </Link>
         <div className={styles.navContainer}>
           <h4>Login</h4>
           <h4>Sign up</h4>
