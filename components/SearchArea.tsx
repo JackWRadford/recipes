ab0e8dda5f6db70b040278026eb2483b1b16b14c
JackR <jack.radford@live.co.uk>
2022-09-24 18:16:03 +0100

    Added reusable Input component. Some other small styling changes

--- components/SearchArea.tsx
+++ components/SearchArea.tsx
@@ -1,9 +1,20 @@
+import { ChangeEvent } from "react";
 import styles from "../styles/SearchArea.module.css";
+import Input from "./shared/Input";
 
 const SearchArea = () => {
   return (
     <div className={styles.wrapper}>
-      <h3 className={styles.title}>Search recipes</h3>
+      <h3 className={styles.title}>Find a new recipe!</h3>
+      <Input
+        type={"search"}
+        name={""}
+        value={""}
+        placeholder={"Search"}
+        onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </div>
   );
 };
---
+++ components/shared/Input.tsx
@@ -0,0 +1,31 @@
+import { ChangeEventHandler, FC, HTMLInputTypeAttribute } from "react";
+import styles from "../../styles/Input.module.css";
+
+interface InputProps {
+  type: HTMLInputTypeAttribute;
+  name: string;
+  value: string | number;
+  placeholder: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+const Input: FC<InputProps> = ({
+  type,
+  name,
+  value,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <input
+      className={styles.input}
+      type={type}
+      value={value}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
