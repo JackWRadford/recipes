f33a6eba0b838dda104b90751915d0411ebc15c2
JackR <jack.radford@live.co.uk>
2022-09-30 18:17:18 +0100

    Added confirm delete modal and implemented delete

--- components/recipePage/ActionsRow.tsx
+++ components/recipePage/ActionsRow.tsx
@@ -1,8 +1,11 @@
+import { deleteDoc, doc } from "firebase/firestore/lite";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { db } from "../../firebaseConfig";
 import { Recipe } from "../../models/Recipe";
 import styles from "../../styles/ActionsRow.module.css";
 import Button from "../shared/Button";
+import ConfirmDeleteModal from "./ConfirmDeleteModal";
 
 interface IActionsRow {
   recipe: Recipe;
@@ -10,14 +13,26 @@ interface IActionsRow {
 
 const ActionsRow: FC<IActionsRow> = ({ recipe }) => {
   const router = useRouter();
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const onDeleteHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const onCloseHandler = () => {
+    setShowConfirmModal(false);
+  };
 
   return (
     <div className={styles.wrapper}>
+      {showConfirmModal && (
+        <ConfirmDeleteModal onClose={onCloseHandler} recipe={recipe} />
+      )}
       <Button
         type={"button"}
         name={"deleterecipe"}
         label={"Delete"}
-        onClick={() => {}}
+        onClick={onDeleteHandler}
       />
       <Button
         type={"button"}
---
+++ components/recipePage/ConfirmDeleteModal.tsx
@@ -0,0 +1,57 @@
+import { deleteDoc, doc } from "firebase/firestore/lite";
+import { FC, useState } from "react";
+import { db } from "../../firebaseConfig";
+import { Recipe } from "../../models/Recipe";
+import Button from "../shared/Button";
+import Modal from "../shared/Modal";
+import styles from "../../styles/ConfirmDeleteModal.module.css";
+
+interface IConfirmDeleteModalProps {
+  onClose: () => void;
+  recipe: Recipe;
+}
+
+const ConfirmDeleteModal: FC<IConfirmDeleteModalProps> = ({
+  onClose,
+  recipe,
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDeleteHandler = async () => {
+    setIsLoading(true);
+    const docRef = doc(db, "recipes", recipe.id!);
+    try {
+      await deleteDoc(docRef);
+      setIsLoading(false);
+      onClose();
+    } catch (error) {
+      setIsLoading(false);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <Modal
+      title={"Confirm recipe deletion"}
+      onClose={onClose}
+      content={
+        <div className={styles.wrapper}>
+          <p>
+            {
+              "Are you sure? Once deleted you cannot recover this recipes's data!"
+            }
+          </p>
+          <Button
+            type={"button"}
+            name={"delete"}
+            label={"Delete"}
+            onClick={onDeleteHandler}
+            isLoading={isLoading}
+          />
+        </div>
+      }
+    />
+  );
+};
+
+export default ConfirmDeleteModal;
