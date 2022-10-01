04ff29803713b05b4d80cbded4e8772c06dfa415
JackR <jack.radford@live.co.uk>
2022-10-01 01:37:04 +0100

    Fixed recipe duration string

--- helper/ConvertionHelpers.ts
+++ helper/ConvertionHelpers.ts
@@ -2,7 +2,7 @@
 const secondsToHoursMinutes = (seconds: number): string => {
   const hours = Math.trunc(seconds / 3600);
   const minutes = Math.trunc((seconds % 3600) / 60);
-  return hours ? `${hours}h` : "" + `${minutes}m`;
+  return `${hours ? `${hours}h` : ""} ${minutes}m`;
 };
 
 export { secondsToHoursMinutes };
