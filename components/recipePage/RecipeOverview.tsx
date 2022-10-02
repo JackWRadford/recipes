41dd0f9427c4d74c8e209b626dd0f21fbbeffe97
JackR <jack.radford@live.co.uk>
2022-10-02 22:19:30 +0100

    Added cooking time and difficulty to recipe page

--- components/recipePage/RecipeOverview.tsx
+++ components/recipePage/RecipeOverview.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { secondsToHoursMinutes } from "../../helper/ConvertionHelpers";
 import { Recipe } from "../../models/Recipe";
 import styles from "../../styles/RecipeOverview.module.css";
 
@@ -13,6 +14,9 @@ const RecipeOverview: FC<IRecipeOverviewProps> = ({ recipe }) => {
         <h1 className={styles.title}>{recipe.name}</h1>
         <h4 className={styles.author}>{`By ${recipe.author}`}</h4>
         <p>{recipe.description}</p>
+        <p className={styles.otherDetails}>{`Time: ${secondsToHoursMinutes(
+          recipe.cookingTime
+        )} • Difficulty: ${recipe.difficulty}`}</p>
       </div>
       {/* <div className={styles.imageContainer}></div> */}
     </div>
