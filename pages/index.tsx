07fad6e6f8d8cfb4ad7eac836403167a6ccd4ab3
JackR <jack.radford@live.co.uk>
2022-10-03 15:03:14 +0100

    Completed load more button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
-import { collection, getDocs } from "firebase/firestore/lite";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  startAfter,
+} from "firebase/firestore/lite";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
@@ -11,16 +19,23 @@ import styles from "../styles/HomePage.module.css";
 
 const HomePage: NextPage = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [lastRecipe, setLastRecipe] = useState<QueryDocumentSnapshot<Recipe>>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     /// Get recipes from firestore
     const fetchRecipes = async () => {
       console.log("FIRESTORE: fetchRecipes");
-      const recipesCol = collection(db, "recipes").withConverter(
-        recipeConverter
+      const recipesQueryFirst = query(
+        collection(db, "recipes").withConverter(recipeConverter),
+        orderBy("dateCreated", "desc"),
+        limit(6)
       );
-      const recipesSnapshot = await getDocs(recipesCol);
-      const recipesList = recipesSnapshot.docs.map((doc) => doc.data());
+      const recipesSnapshots = await getDocs(recipesQueryFirst);
+      const lastVisible =
+        recipesSnapshots.docs[recipesSnapshots.docs.length - 1];
+      const recipesList = recipesSnapshots.docs.map((doc) => doc.data());
+      setLastRecipe(lastVisible);
       setRecipes(recipesList);
       return recipesList;
     };
@@ -28,11 +43,34 @@ const HomePage: NextPage = () => {
     fetchRecipes();
   }, []);
 
+  const loadMore = async () => {
+    if (!lastRecipe) return;
+    setIsLoading(true);
+    console.log("FIRESTORE: fetchRecipes");
+    const recipesQueryNext = query(
+      collection(db, "recipes").withConverter(recipeConverter),
+      orderBy("dateCreated", "desc"),
+      startAfter(lastRecipe),
+      limit(6)
+    );
+    const recipesSnapshots = await getDocs(recipesQueryNext);
+    const lastVisible = recipesSnapshots.docs[recipesSnapshots.docs.length - 1];
+    const recipesList = recipesSnapshots.docs.map((doc) => doc.data());
+    setLastRecipe(lastVisible);
+    setRecipes((oldRecipes) => [...oldRecipes, ...recipesList]);
+    setIsLoading(false);
+  };
+
   return (
     <>
       <Header />
       <div className={styles.contentWrapper}>
-        <RecipesList recipes={recipes} />
+        <RecipesList
+          recipes={recipes}
+          loadMore={loadMore}
+          noMoreRecipes={typeof lastRecipe === "undefined"}
+          isLoading={isLoading}
+        />
         <SearchArea />
       </div>
       <Footer />
